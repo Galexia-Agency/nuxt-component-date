21ef623712edf203291198a29e08d70a34b9cd03
Joe <joe@joebailey.xyz>
2023-11-20 14:32:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Galexia Nuxt Component Date
+
+## Install
+
+### Pre-Requisites
+
+We assume you have a Nuxt project.
+
+### You can now install this package
+
+```bash
+yarn add https://github.com/Galexia-Agency/nuxt-component-date
+```
+
+```js
+// plugins/galexia/nuxt-components/date.js
+import Vue from 'vue'
+import Galexia_NuxtComponent_Date from 'nuxt-component-date/index.vue'
+
+Vue.component('GalexiaDate', Galexia_NuxtComponent_Date)
+```
+
+```js
+// nuxt.config.js
+...
+plugins: [
+  '~plugins/galexia/components/date.js'
+],
+...
+```
+
+### And use it like so
+
+```js
+// pages/index.js
+<GalexiaDate
+  class="post__date"
+  :date="post.date"
+  day-text-color="#ffffff"
+  day-background-color="#000000"
+  month-year-text-color="#ffffff"
+  month-year-background-color="#333333"
+/>
+```
---
+++ index.vue
@@ -0,0 +1,98 @@
+<style scoped>
+.date {
+  grid-row: span 3;
+  grid-column: 1;
+  margin-top: 8px;
+  text-align: center
+}
+.day {
+  display: grid;
+  width: 65px;
+  height: 40px;
+  color: var(--dayTextColor);
+  font-size: 1.5rem;
+  font-style: italic;
+  background-color: var(--dayBackgroundColor);
+  place-items: center
+}
+.monthYear {
+  width: 65px;
+  height: 48px;
+  margin-top: -1px;
+  padding-top: 4px;
+  color: var(--monthYearTextColor);
+  background-color: var(--monthYearBackgroundColor);
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 50% 70%, 0 100%)
+}
+@media (max-width: 576px) {
+  .date {
+    margin-bottom: 1rem
+  }
+}
+</style>
+<template>
+  <div
+    class="date"
+    :style="`
+      --dayTextColor: ${dayTextColor};
+      --dayBackgroundColor: ${dayBackgroundColor};
+      --monthYearTextColor: ${monthYearTextColor};
+      --monthYearBackgroundColor: ${monthYearBackgroundColor};
+    `"
+  >
+    <div class="day" v-text="getDay(date)" />
+    <div class="monthYear" v-text="`${getMonth(date)} '${getYear(date)}`" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GalexiaNuxtComponentDate',
+  props: {
+    date: {
+      type: Date,
+      required: true
+    },
+    dayTextColor: {
+      type: String,
+      required: true
+    },
+    dayBackgroundColor: {
+      type: String,
+      required: true
+    },
+    monthYearTextColor: {
+      type: String,
+      required: true
+    },
+    monthYearBackgroundColor: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    getDay (date) {
+      return date.getDate()
+    },
+    getMonth (date) {
+      const monthArr = []
+      monthArr[0] = 'Jan'
+      monthArr[1] = 'Feb'
+      monthArr[2] = 'Mar'
+      monthArr[3] = 'Apr'
+      monthArr[4] = 'May'
+      monthArr[5] = 'Jun'
+      monthArr[6] = 'Jul'
+      monthArr[7] = 'Aug'
+      monthArr[8] = 'Sep'
+      monthArr[9] = 'Oct'
+      monthArr[10] = 'Nov'
+      monthArr[11] = 'Dec'
+      return monthArr[date.getMonth()]
+    },
+    getYear (date) {
+      return date.getFullYear().toString().substring(2)
+    }
+  }
+}
+</script>
